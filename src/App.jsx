2e9dff087470a02610ea115decc6d3a09a4d8fd4
Imon <contact@imonweb.co.uk>
2024-01-23 00:28:49 +0000

    react-context-theme-toggler

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import styled, { createGlobalStyle }  from 'styled-components'
+import Toggler from './Toggler'
+import ThemeContext from './ThemeContext'
 import './App.css'
+import { useState } from 'react'
+
+const GlobalStyles = createGlobalStyle`
+  body {
+    background-color: ${props => props.darkMode ? '#334' : '#eef'};
+    color: ${props => props.darkMode ? '#eee' : '#222'}
+  }
+`;
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  
+const [darkMode, setDarkMode] = useState(false);
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <ThemeContext.Provider value={{darkMode, setDarkMode}}>
+          <GlobalStyles darkMode={darkMode} />
+          <h3>Dark Mode:</h3>
+          <Toggler />
+        </ThemeContext.Provider>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
---
+++ src/Toggler.jsx
@@ -0,0 +1,57 @@
+import React, { useContext } from 'react'
+import styled from 'styled-components'
+import { MoonIcon } from '@heroicons/react/solid'
+import ThemeContext from './ThemeContext'
+
+const StyledLabel = styled.label `
+  input {
+    position: fixed;
+    left: -999999px;
+  }
+
+  input ~ div {
+    width: 30px;
+    height: 16px;
+    background: #eee;
+    border-radius: 8px;
+    position: relative;
+  }
+
+  svg {
+    height: 14px;
+    color: #fff;
+    background-color: #ccc;
+    border-radius: 7px;
+    position: absolute;
+    top: 1px;
+    left: 1px;
+    transition: all .4s ease;
+  }
+
+  input:checked ~ div {
+    background: #668;
+    svg {
+      background-color: #224;
+      left: 15px;
+    }
+  }
+
+`
+
+const Toggler = () => {
+  const theme = useContext(ThemeContext)
+
+  return (
+    <StyledLabel>
+      <input 
+        type="checkbox" 
+        checked={theme.darkMode}
+        onChange={() => theme.setDarkMode(oldValue => !oldValue)}
+      />
+      <div><MoonIcon /></div>
+      
+    </StyledLabel>
+  )
+}
+
+export default Toggler 
